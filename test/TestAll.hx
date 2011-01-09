1d41ebad3ec9fec7150b9683f7065d0f6f40ea1c
Franco Ponticelli <francoponticelli@ep2.lan>
2011-01-09 14:59:01 +0000

    fixed tests

--- test/TestAll.hx
+++ test/TestAll.hx
@@ -5,9 +5,9 @@
 	{
 		var runner = new utest.Runner();
 		
-//		runner.addCase(new htemplate.TestParser());
-//		runner.addCase(new htemplate.TestScriptBuilder());
-//		runner.addCase(new htemplate.TestTemplate());
+		runner.addCase(new htemplate.TestParser());
+		runner.addCase(new htemplate.TestScriptBuilder());
+		runner.addCase(new htemplate.TestTemplate());
 		runner.addCase(new htemplate.TestEnhancedInterp());
 		
 		var report = new utest.ui.text.PrintReport(runner);
